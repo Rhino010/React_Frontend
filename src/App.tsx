906a1fe522d78df3ee8c9962e44d98d28bc4ace7
Ryan Boll <bollr38@hotmail.com>
2024-08-15 11:12:41 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { HashRouter, Routes, Route, Link } from 'react-router-dom';
+import routes from './config/routes';
+
+
+function App() {
+
+  return (
+
+    <HashRouter>
+      {/* Navbar goes here */}
+      <Routes>
+        {routes.map((route: any, index: any) => (
+          <Route
+            key={index}
+            path={route.path}
+            element={
+              <route.component />
+            }
+          />
+        ))}
+      </Routes>
+    </HashRouter>
+  )
+}
+
+export default App
